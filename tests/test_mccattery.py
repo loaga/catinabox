94275e9b4e55e0825eddda14265390de9fbed430
loaga loa <loaga@cox.net>
2016-08-06 18:00:16 -0400

    Add run sh

---
+++ tests/test_mccattery.py
@@ -0,0 +1,43 @@
+import pytest
+
+from catinabox import cattery, mccattery
+
+
+@pytest.fixture(params=[
+    cattery.Cattery,
+    mccattery.McCattery
+])
+def cattery_client(request):
+    return request.param()
+
+
+###########################################################################
+# add_cats
+###########################################################################
+
+def test__add_cats__succeeds(cattery_client):
+    cattery_client.add_cats(["Fluffy", "Snookums"])
+    assert cattery_client.cats == ["Fluffy", "Snookums"]
+    assert cattery_client.num_cats == 2
+
+
+###########################################################################
+# remove_cat
+###########################################################################
+
+def test__remove_cat__succeeds(cattery_client):
+    cattery_client.add_cats(["Fluffy", "Junior"])
+    cattery_client.remove_cat("Fluffy")
+    assert cattery_client.cats == ["Junior"]
+    assert cattery_client.num_cats == 1
+
+
+def test__remove_cat__no_cats__fails(cattery_client):
+    with pytest.raises(cattery.CatNotFound):
+        cattery_client.remove_cat("Fluffles")
+
+
+def test__remove_cat__cat_not_in_cattery__fails(cattery_client):
+    cattery_client.add_cats(["Fluffy"])
+    with pytest.raises(cattery.CatNotFound):
+        cattery_client.remove_cat("Snookums")
